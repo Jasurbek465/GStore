98c19e04456ae47af60087b167a581edd0d91197
Jasurbek465 <abdurashidovjasurbek584@gmail.com>
2025-07-21 11:46:52 +0500

    to'g'irladim

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var swiper = new Swiper(".intro-swiper", {
-      pagination: {
-        el: ".swiper-pagination",
-      },
-    });
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
